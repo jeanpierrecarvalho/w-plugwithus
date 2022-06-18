d1d00227cc8bad9be918a5e7dd96b637d8f0338c
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2022-06-18 17:25:55 +0100

    feat!: storybook instalation

---
+++ NEWREADME.md
@@ -0,0 +1,5 @@
+npx sb init --builder webpack5
+
+npx create-next-app -e <https://github.com/theodorusclarence/ts-nextjs-tailwind-starter> .
+
+curl -s <https://raw.githubusercontent.com/theodorusclarence/expansion-pack/main/storybook/trigger.sh> | bash -s
---
+++ src/components/buttons/__stories__/Button.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import * as React from 'react';
+
+import Button from '@/components/buttons/Button';
+
+export default {
+  title: 'Components/Buttons/Button',
+  component: Button,
+  argTypes: {
+    children: {
+      control: { type: 'text' },
+    },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = { children: 'Hello' };
